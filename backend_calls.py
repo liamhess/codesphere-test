377f3f789010b726dd824e535991d0b0a3877ad8
liam.m.hess <liam.m.hess@gmail.com>
2024-04-28 19:16:21 +0000

    first iteration of frontend

---
+++ backend_calls.py
@@ -0,0 +1,19 @@
+import requests
+import os
+
+backend_address: str = os.environ.get("BACKEND_ADDRESS")
+
+def subscribe_user(email: str, interests: str):
+    body = {
+        "email": email,
+        "interests": interests
+    }
+    response = requests.post(backend_address + "/subscribe", json=body)
+    print(response.status_code)
+    
+def unsubscribe_user(email: str):
+    body = {
+        "email": email
+    }
+    response = requests.post(backend_address + "/unsubscribe", json=body)
+    print(response.status_code)
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import backend_calls
+
+st.set_page_config(
+    page_title="Hacker News Aggregator",
+    page_icon="💌"
+)
 
 st.title("Hacker News Aggregator ✌️")
 st.write("Just input a couple of your interests and your mail to get a daily newsletter with the most relevant Hacker News stories for you")
@@ -6,16 +12,18 @@ st.write("Just input a couple of your interests and your mail to get a daily new
 with st.form(key="Form"):
     st.write("Subscribe to your personalized Hacker News Aggregator")
     email: str = st.text_input(label="E-Mail", placeholder="email@domain.com")
-    interests: str = st.text_input(label="Interests", placeholder="Next-Gen Cloud Providers, AI, Cybersecurity, Codesphere")
+    interests: str = st.text_input(label="Interests", placeholder="AI, Cloud, Web Development, DevOps, Cybersecurity, Analytics...")
     submit_button: bool = st.form_submit_button(label="Submit")
-	
+        
 if submit_button:
-	st.write("Subscribed!!!")
+    backend_calls.subscribe_user(email, interests)
+    st.write("Subscribed!!!")
 
 with st.form(key="Unsubscribe-Form"):
     st.write("Unsubscribe your E-Mail")
     email: str = st.text_input(label="E-Mail", placeholder="email@domain.com")
     submit_button: bool = st.form_submit_button(label="Submit")
-	
+        
 if submit_button:
-	st.write("Unsubscribed!!!")
+    backend_calls.unsubscribe_user(email)
+    st.write("Unsubscribed!!!")
